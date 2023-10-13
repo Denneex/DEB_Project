16ef41c238f5121198d4111a79ab9ab4f522a1f0
Denneex <72454893+Denneex@users.noreply.github.com>
2023-10-13 22:29:51 +0100

    Made changes to DAG upload_to_postgres

--- dag/upload_to_postgres_local.py
+++ dag/upload_to_postgres_local.py
@@ -28,7 +28,7 @@ with DAG(
     upload_movies_reviews = LocalFilesystemToGCSOperator(
         task_id='upload_movies_reviews',
         src=os.path.join("C:", "Users", "shopinverse", "Documents", "DATA-ENGINEERING", "movie_review.csv"),
-        dst='movies_reviews.csv',
+        dst='movies_review.csv',
         bucket='deb-bucket',
         mime_type='text/csv',
         gcp_conn_id='gcp_conn_id',
@@ -37,7 +37,7 @@ with DAG(
     # Define the task to upload log_reviews.csv to gcp bucket
     upload_log_reviews = LocalFilesystemToGCSOperator(
         task_id='upload_log_reviews',
-        src=os.path.join("C:", "Users", "shopinverse", "Documents", "DATA-ENGINEERING", "log_reviews - log_reviews.csv"),
+        src=os.path.join("C:", "Users", "shopinverse", "Documents", "DATA-ENGINEERING", "log_reviews.csv"),
         dst='log_reviews.csv',
         bucket='deb-bucket',
         mime_type='text/csv',
@@ -70,7 +70,7 @@ with DAG(
         sql="""
             COPY deb_schema.user_purchase FROM %s DELIMITER ',' CSV HEADER;
             """,
-        parameters=[os.path.join("C:", "Users", "shopinverse", "Documents", "DATA-ENGINEERING", "user_purchase - user_purchase.csv")],
+        parameters=[os.path.join("C:", "Users", "shopinverse", "Documents", "DATA-ENGINEERING", "user_purchase.csv")],
     )
 
     # Define the dependencies between the tasks
